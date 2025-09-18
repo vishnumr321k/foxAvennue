47d8ea1e35c376a513f8e4ae75a9ac23db3db533
VISHNU M R <vishnumr321k@gmail.com>
2025-09-18 11:45:06 +0530

    register DTo created

---
+++ backend/src/auth/dto/register.dto.ts
@@ -0,0 +1,16 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
+
+export class UserRegisterDto{
+    @IsNotEmpty()
+    @IsString()
+    name: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @IsEmail()
+    email: string;
+
+    @IsNotEmpty()
+    @MinLength(6)
+    password: string;
+}
