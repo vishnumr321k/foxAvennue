fe77ffe930343093a86d49363384aa973e3c6a09
VISHNU M R <vishnumr321k@gmail.com>
2025-09-20 14:37:51 +0530

    Product Schema creation complete

---
+++ backend/src/products/products.schema.ts
@@ -0,0 +1,30 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type ProductDocument = Product & Document;
+
+@Schema({ timestamps: true })
+export class Product {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop()
+  description: string;
+
+  @Prop({ required: true })
+  price: number;
+
+  @Prop({ required: true, default: 0 })
+  stock: number;
+
+  @Prop({ type: [String], default: [] })
+  sizes: string[];
+
+  @Prop({ type: [String], default: [] })
+  images: string[];
+
+  @Prop({ default: true })
+  isActive: boolean;
+}
+
+export const ProductSchema = SchemaFactory.createForClass(Product);
